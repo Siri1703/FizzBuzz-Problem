8d9aa033700b65cad519573e1223c2254f323667
Siri1703 <sirilakshmi1703@gmail.com>
2021-08-19 18:46:05 +0530

    Requirement 1: Divisibility by 3(Fizz) or Divisibility by 5(Buzz)

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,18 @@
+import java.util.*;
 public class FizzBuzz {
+    int number;
+    public FizzBuzz(int n) {
+        this.number=n;
+    }
+
+    public String fizzbuzz() {
+        while(true)
+        {
+            if(number%3==0)
+                return  "Fizz";
+            if(number%5==0)
+                return "Buzz";
+
+        }
+    }
 }
--- src/test/java/FizzBuzzTesting.java
+++ src/test/java/FizzBuzzTesting.java
@@ -1,2 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTesting {
+    @Test
+    public void toObtainFizzIfNumberIsDivisibleByThree()
+    {
+        FizzBuzz obj=new FizzBuzz(6);
+        String expectedOutput="Fizz";
+        String actualOutput=obj.fizzbuzz();
+        assertEquals(expectedOutput,actualOutput);
+    }
+
+    @Test
+    public void toObtainBuzzIfNumberIsDivisibleByFive()
+    {
+        FizzBuzz obj=new FizzBuzz(10);
+        String expectedOutput="Buzz";
+        String actualOutput=obj.fizzbuzz();
+        assertEquals(expectedOutput,actualOutput);
+    }
+
+
 }
