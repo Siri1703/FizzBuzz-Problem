5e26f6d87683b99ec887439eadc1d28d68f6ee3b
Siri1703 <sirilakshmi1703@gmail.com>
2021-08-19 19:07:42 +0530

    Requirement 2: Divisibility by 15,number contains 3 or 5

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -6,12 +6,23 @@ public class FizzBuzz {
     }
 
     public String fizzbuzz() {
+        String k=Integer.toString(number);
         while(true)
         {
+            if(number%15==0)
+                return "FizzBuzz";
             if(number%3==0)
                 return  "Fizz";
             if(number%5==0)
                 return "Buzz";
+            if( k.contains("3") && k.contains("5"))
+                return "FizzBuzz";
+            if(k.contains("3"))
+                return "Fizz";
+            if(k.contains("5"))
+                return "Buzz";
+
+            return null;
 
         }
     }
--- src/test/java/FizzBuzzTesting.java
+++ src/test/java/FizzBuzzTesting.java
@@ -21,5 +21,32 @@ public class FizzBuzzTesting {
         assertEquals(expectedOutput,actualOutput);
     }
 
+    @Test
+    public void toObtainFizzIfNumberContainsThree()
+    {
+        FizzBuzz obj=new FizzBuzz(23);
+        String expectedOutput="Fizz";
+        String actualOutput=obj.fizzbuzz();
+        assertEquals(expectedOutput,actualOutput);
+    }
+
+    @Test
+    public void toObtainBuzzIfNumberContainsFive()
+    {
+        FizzBuzz obj=new FizzBuzz(52);
+        String expectedOutput="Buzz";
+        String actualOutput=obj.fizzbuzz();
+        assertEquals(expectedOutput,actualOutput);
+    }
+
+    @Test
+    public void toObtainBuzzIfNumberIsDivisibleByFifteen()
+    {
+        FizzBuzz obj=new FizzBuzz(30);
+        String expectedOutput="FizzBuzz";
+        String actualOutput=obj.fizzbuzz();
+        assertEquals(expectedOutput,actualOutput);
+    }
+
 
 }
